d63eae2d437ada663993a7d1bc2e3bcd873a580a
Davit <davit.janashia1@gmail.com>
2020-07-21 17:00:32 +0200

    qq

--- js/script5.js
+++ js/script5.js
@@ -1,45 +1,48 @@
 $(document).ready(init);
 
 function init(){
-
+  myClickSquare();
 }
 // ************************************************************
 
-var square = $('.my-square');
-square.click(function(){
-  var mySquare = $(this);
-  mySquare.html('');
-  getRandomNumber();
+function myClickSquare(){
+  var square = $('.my-square');
+  square.click(function(){
+    var mySquare = $(this);
+    mySquare.html('');
+    getRandomNumber();
 
 
-  function getRandomNumber(){
-    $.ajax({
-      url: 'https://flynn.boolean.careers/exercises/api/random/int',
-      method: 'GET',
-      success: function (data, success){
-        // console.log(data);
-        var numRand = data['response'];
-        // console.log(numRand);
-        mySquare.append(numRand);
+    function getRandomNumber(){
+      $.ajax({
+        url: 'https://flynn.boolean.careers/exercises/api/random/int',
+        method: 'GET',
+        success: function (data, success){
+          // console.log(data);
+          var numRand = data['response'];
+          // console.log(numRand);
+          mySquare.append(numRand);
 
-        if (numRand < 5) {
-          mySquare.removeClass('green yellow');
-          mySquare.addClass('green');
-        } else {
-          mySquare.removeClass('green yellow');
-          mySquare.addClass('yellow');
+          if (numRand < 5) {
+            mySquare.removeClass('green yellow');
+            mySquare.addClass('green');
+          } else {
+            mySquare.removeClass('green yellow');
+            mySquare.addClass('yellow');
+          }
+        },
+
+        error: function (request, state, error) {
+          console.log('error!');
+          console.log(request);
+          console.log(state);
+          console.log(error);
         }
-      },
+      });
+    }
+  });
+}
 
-      error: function (request, state, error) {
-        console.log('error!');
-        console.log(request);
-        console.log(state);
-        console.log(error);
-      }
-    });
-  }
-});
 
 
 
